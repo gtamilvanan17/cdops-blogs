368494cebbb675b6f9edbc5b4fea39fc1359125a
gtamilvanan17 <tamilbecse139@gmail.com>
2024-05-29 13:34:57 +0530

    added-basic-page

---
+++ src/config.ts
@@ -0,0 +1,75 @@
+import type {
+  LicenseConfig,
+  NavBarConfig,
+  ProfileConfig,
+  SiteConfig,
+} from './types/config'
+import { LinkPreset } from './types/config'
+
+export const siteConfig: SiteConfig = {
+  title: 'CD OPS',
+  subtitle: 'Cloud and Devops | Blogs',
+  lang: 'en', // 'en', 'zh_CN', 'zh_TW', 'ja'
+  themeColor: {
+    hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: false, // Hide the theme color picker for visitors
+  },
+  banner: {
+    enable: false,
+    src: 'assets/images/demo-banner.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  },
+  favicon: [
+    // Leave this array empty to use the default favicon
+    // {
+    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    // }
+  ],
+}
+
+export const navBarConfig: NavBarConfig = {
+  links: [
+    LinkPreset.Home,
+    LinkPreset.Archive,
+    LinkPreset.About,
+    // {
+    //   name: 'GitHub',
+    //   url: 'https://github.com/saicaca/fuwari', // Internal links should not include the base path, as it is automatically added
+    //   external: true, // Show an external link icon and will open in a new tab
+    // },
+  ],
+}
+
+export const profileConfig: ProfileConfig = {
+  avatar: 'assets/images/image.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: 'CD OPS',
+  bio: 'The Internet has no eraser.',
+  links: [
+    // You will need to install the corresponding icon set if it's not already included
+    // `pnpm add @iconify-json/<icon-set-name>`
+    // Visit https://icones.js.org/ for icon codes
+
+    {
+      name: 'Twitter',
+      icon: 'fa6-brands:twitter',
+      url: 'https://twitter.com/GTamilvananOff',
+    },
+    {
+      name: 'Linked In',
+      icon: 'fa6-brands:linkedin',
+      url: 'https://linkedin.com/in/gtamilvanan17',
+    },
+    {
+      name: 'Instagram',
+      icon: 'fa6-brands:instagram',
+      url: 'https://instagram.com/cdops_official',
+    },
+  ],
+}
+
+export const licenseConfig: LicenseConfig = {
+  enable: true,
+  name: 'CC BY-NC-SA 4.0',
+  url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+}
---
+++ src/content/spec/about.md
@@ -0,0 +1,18 @@
+# About
+<!-- This is the demo site for [Fuwari](https://github.com/saicaca/fuwari). -->
+This site is purely about posting blogs related to linux, devops, cloud computing, networking and all about technologies.
+
+::github{repo="gtamilvanan17/blog"}
+
+<!-- > ### Sources of images used in this site
+> - [Unsplash](https://unsplash.com/)
+> - [星と少女](https://www.pixiv.net/artworks/108916539) by [Stella](https://www.pixiv.net/users/93273965)
+> - [Rabbit - v1.4 Showcase](https://civitai.com/posts/586908) by [Rabbit_YourMajesty](https://civitai.com/user/Rabbit_YourMajesty) -->
+
+> ### Follow us on below mediums.
+> - [Instagram](https://instagram.com/cdops_official)
+> - [Hashnode](#)
+> - [Medium](#)
+> - [Hashnode](#)
+> - [Linked In](https://linkedin.com/in/gtamilvanan17)
+> - [Twitter](https://x.com/GTamilvananOff)
