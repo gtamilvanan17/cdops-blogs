f6b1808f35556397371cf3cc167be95fb6d10fca
gtamilvanan17 <tamilbecse139@gmail.com>
2024-10-01 18:03:39 +0530

    disable the license config

--- src/config.ts
+++ src/config.ts
@@ -85,7 +85,7 @@ export const profileConfig: ProfileConfig = {
 }
 
 export const licenseConfig: LicenseConfig = {
-  enable: true,
+  enable: false,
   name: 'CC BY-NC-SA 4.0',
   url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
 }
